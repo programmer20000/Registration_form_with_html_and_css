bb22451e81559fe2ebf95bc7680ffa041c146584
progrmmer <84572292+programmer20000@users.noreply.github.com>
2022-04-20 15:08:14 +0300

    was add files project

---
+++ app/src/main/java/com/example/registration_form_with_html_and_css/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.registration_form_with_html_and_css;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.webView);
+        webView.loadUrl("assets\\html\\index.html");
+    }
+}
